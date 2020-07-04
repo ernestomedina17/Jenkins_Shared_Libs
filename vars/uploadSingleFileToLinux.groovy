f51105a99ae9c7fc079c38213d3799f7a144ec63
ernestomedina17@gmail.com <ernestomedina17@gmail.com>
2020-07-04 13:25:29 -0500

    updating pass

--- vars/uploadSingleFileToLinux.groovy
+++ vars/uploadSingleFileToLinux.groovy
@@ -1,5 +1,5 @@
 def call() {    
-    sshPublisher(publishers: [sshPublisherDesc(configName: 'localhost', sshCredentials: [encryptedPassphrase: '{AQAAABAAAAAQF9+CxWBdzVxVWFOW/0JePp36kdT68Svgc0LTSkX4uAM=}', 
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'localhost', sshCredentials: [encryptedPassphrase: '{AQAAABAAAAAQkMeyfvLn1dSSUCd5D3xnXqdu6hxK77+YLyz5vqXV8c8=}', 
     key: '', keyPath: '', username: 'ernesto'], transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'ls -l ', execTimeout: 120000, flatten: false, 
     makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'directoryA/directoryB/', 
     sourceFiles: 'directoryA/directoryB/config.props')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
