b7787cd10ce08b5c6371747b9fda66d6b59fe501
ernestomedina17@gmail.com <ernestomedina17@gmail.com>
2020-07-04 19:42:41 -0500

    getting BapSshPublisherDescriptor

--- vars/uploadSingleFileToLinux.groovy
+++ vars/uploadSingleFileToLinux.groovy
@@ -1,38 +1,38 @@
-import jenkins.plugins.publish_over_ssh.BapSshHostConfiguration
+import jenkins.plugins.publish_over_ssh.*
+import jenkins.plugins.publish_over_ssh.descriptor.*
 
 def call() {
 
-    def server = new BapSshHostConfiguration()
-    server.setName('server01')
-    server.setHostname('192.168.0.22')
-    server.setPort(22)
-    server.setTimeout(60000)
-    
+    def hostconf = new BapSshHostConfiguration()
+    hostconf.setName('server01')
+    hostconf.setHostname('192.168.0.22')
+    hostconf.setPort(22)
+    hostconf.setTimeout(60000)
 
-    sshPublisher(
+    Jenkins.instance.getDescriptorByType(BapSshPublisherDescriptor.class).getPublisherPluginDescriptor().addHostConfiguration(hostconf)
+
+    try { 
+        sshPublisher(
         publishers: 
             [sshPublisherDesc(
                 configName: server.getName(), 
                 sshCredentials: [
                     encryptedPassphrase: '{AQAAABAAAAAQkMeyfvLn1dSSUCd5D3xnXqdu6hxK77+YLyz5vqXV8c8=}',
-                    key: '', 
-                    keyPath: '', 
-                    username: 'ernesto'], 
+                    key: '', keyPath: '', username: 'ernesto'], 
                 transfers: [
                     sshTransfer(
-                        cleanRemote: false, 
-                        excludes: '', 
-                        execCommand: 'ls -l ', 
-                        execTimeout: 120000, 
-                        flatten: false, 
-                        makeEmptyDirs: false, 
-                        noDefaultExcludes: false, 
-                        patternSeparator: '[, ]+', 
-                        remoteDirectory: '', 
-                        remoteDirectorySDF: false, 
+                        cleanRemote: false, excludes: '', execCommand: 'ls -l ', 
+                        execTimeout: 120000, flatten: false, makeEmptyDirs: false, 
+                        noDefaultExcludes: false, patternSeparator: '[, ]+', 
+                        remoteDirectory: '', remoteDirectorySDF: false, 
                         removePrefix: 'directoryA/directoryB/', 
                         sourceFiles: 'directoryA/directoryB/config.props')], 
                 usePromotionTimestamp: false, 
                 useWorkspaceInPromotion: false, 
-                verbose: false)])
+                verbose: false)]) 
+    } catch(Exception ex) { 
+        println ex
+    } finally {
+        Jenkins.instance.getDescriptorByType(BapSshPublisherDescriptor.class).getPublisherPluginDescriptor().removeHostConfiguration(hostconf.name)
+    }
 }
